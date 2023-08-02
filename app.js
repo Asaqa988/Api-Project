6afbc2e8797c5a2e997b9757c7ea91d701af30dd
Abedalraheem <85874863+Asaqa988@users.noreply.github.com>
2023-08-02 16:29:24 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,27 @@ let books = [
   { id: 3, title: 'Book 3', author: 'Author 3' },
 ];
 
+// In-memory data storage for courses
+let courses = [
+  { id: 1, title: 'Course 1', author: 'Abedalraheem Alsaqqa' },
+  { id: 2, title: 'Course 2', author: 'Abedalraheem Alsaqqa' },
+  { id: 3, title: 'Course 3', author: 'Abedalraheem Alsaqqa' },
+  { id: 4, title: 'Quality Assurance Manual', author: 'Abedalraheem Alsaqqa' },
+  { id: 5, title: 'Web Development Fundamentals', author: 'Abedalraheem Alsaqqa' },
+  { id: 6, title: 'Python Programming Basics', author: 'Abedalraheem Alsaqqa' },
+  { id: 7, title: 'Introduction to Data Science', author: 'Abedalraheem Alsaqqa' },
+  { id: 8, title: 'JavaScript for Beginners', author: 'Abedalraheem Alsaqqa' },
+  { id: 9, title: 'Machine Learning Foundations', author: 'Abedalraheem Alsaqqa' },
+  { id: 10, title: 'Software Engineering Principles', author: 'Abedalraheem Alsaqqa' },
+  { id: 11, title: 'Graphic Design Fundamentals', author: 'Abedalraheem Alsaqqa' },
+  { id: 12, title: 'Data Structures and Algorithms', author: 'Abedalraheem Alsaqqa' },
+  { id: 13, title: 'Mobile App Development with React Native', author: 'Abedalraheem Alsaqqa' },
+  { id: 14, title: 'Introduction to Artificial Intelligence', author: 'Abedalraheem Alsaqqa' },
+  { id: 15, title: 'Ethical Hacking and Cybersecurity', author: 'Abedalraheem Alsaqqa' },
+  // Add more sample data as needed
+];
+
+
 // GET all books
 app.get('/api/books', (req, res) => {
   res.json(books);
@@ -57,9 +78,54 @@ app.delete('/api/books/:id', (req, res) => {
   res.json({ message: 'Book deleted successfully' });
 });
 
+// GET all courses
+app.get('/api/courses', (req, res) => {
+  res.json(courses);
+});
+
+// GET a specific course by ID
+app.get('/api/courses/:id', (req, res) => {
+  const courseId = parseInt(req.params.id);
+  const course = courses.find((course) => course.id === courseId);
+
+  if (!course) {
+    res.status(404).json({ message: 'Course not found' });
+  } else {
+    res.json(course);
+  }
+});
+
+// POST a new course
+app.post('/api/courses', (req, res) => {
+  const newCourse = req.body;
+  newCourse.id = courses.length + 1;
+  courses.push(newCourse);
+  res.status(201).json(newCourse);
+});
+
+// PUT (update) an existing course by ID
+app.put('/api/courses/:id', (req, res) => {
+  const courseId = parseInt(req.params.id);
+  const updatedCourse = req.body;
+  const index = courses.findIndex((course) => course.id === courseId);
+
+  if (index === -1) {
+    res.status(404).json({ message: 'Course not found' });
+  } else {
+    courses[index] = { ...courses[index], ...updatedCourse };
+    res.json(courses[index]);
+  }
+});
+
+// DELETE a course by ID
+app.delete('/api/courses/:id', (req, res) => {
+  const courseId = parseInt(req.params.id);
+  courses = courses.filter((course) => course.id !== courseId);
+  res.json({ message: 'Course deleted successfully' });
+});
+
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
-
